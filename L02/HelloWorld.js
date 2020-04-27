660946330807f354972b1c32fa88af8fc778be58
Okappa <48796514+LukasBrauschHFU@users.noreply.github.com>
2020-04-27 12:12:56 +0200

    Add files via upload

---
+++ L02/HelloWorld.js
@@ -0,0 +1,14 @@
+"use strict";
+var HelloWorld;
+(function (HelloWorld) {
+    clock();
+    function clock() {
+        let time = new Date();
+        let hours = time.getHours();
+        let minutes = time.getMinutes();
+        let seconds = time.getSeconds();
+        console.log("Hello World");
+        console.log("Website visited on:", time, hours, ":", minutes, ":", seconds);
+    }
+})(HelloWorld || (HelloWorld = {}));
+//# sourceMappingURL=HelloWorld.js.map
