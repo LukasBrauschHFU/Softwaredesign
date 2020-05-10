38e0b3700a3057e7165aaad38a2529800598f0ef
Okappa <48796514+LukasBrauschHFU@users.noreply.github.com>
2020-05-10 12:00:47 +0200

    Add files via upload

--- Gedicht/main.ts
+++ Gedicht/main.ts
@@ -2,11 +2,10 @@ namespace Zufallsgedicht {
     let subjects: string[] = ["Harry", "Hermine"];
     let verbs: string[] = ["braut", "liebt"];
     let objects: string[] = ["Zaubertränke", "den Grimm"];
-    let verses: string[] = ["My name is Yoshikage Kira. I'm 33 years old. My house is in the northeast section of Morioh, where all the villas are, and I am not married. I work as an employee for the Kame Yu department stores, and I get home every day by 8 PM at the latest. I don't smoke, but I occasionally drink.I'm in bed by 11 PM, and make sure I get eight hours of sleep, no matter what. After having a glass of warm milk and doing about twenty minutes of stretches before going to bed, I usually have no problems sleeping until morning. Just like a baby, I wake up without any fatigue or stress in the morning. I was told there were no issues at my last check-up.I'm trying to explain that I'm a person who wishes to live a very quiet life. I take care not to trouble myself with any enemies, like winning and losing, that would cause me to lose sleep at night. That is how I deal with society, and I know that is what brings me happiness. Although, if I were to fight I wouldn't lose to anyone."];
+    let verses: string[] = [""];
     let i: number = 0;
     if (subjects.length != 0 || verbs.length != 0 || objects.length != 0) {
-        let newverse: string[] = getVerse();
-        verses.splice( i, 0, newverse );
+        verses.push(getVerse());
         console.log(verses[i]);
         i++;
     }
@@ -15,8 +14,8 @@ namespace Zufallsgedicht {
     }
 
 
-    function getVerse(): string[] {
-        let newverse: string[];
+    function getVerse(): string {
+        let newverse: string;
         let picker: number = Math.floor(Math.random() * subjects.length - 1); //random Index between 0 and subjects.length-1; 
 
         newverse = subjects.splice(picker, 1);
